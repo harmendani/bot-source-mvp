57dca942913088ab7c8e7a7a8f0b83c6633670cf
Pedro Harmendani <pedrohnh@hotmail.com>
2026-08-22 21:34:53 -0300

    chore: unit fastify with yarn v4 and tsv7

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+//@ts-nocheck
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true
+})
+// CommonJs
+const fastify = require('fastify')({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'bot ok!' })
+})
+
+// Run the server!
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
